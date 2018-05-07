9f4048dc440c539968780798ff66134cb31eec59
NancyGomez <nagomez@csumb.edu>
2018-05-07 06:58:07 +0000

    Added the label to the display

--- cylc_webapp/views.py
+++ cylc_webapp/views.py
@@ -10,13 +10,38 @@ from django.http import HttpResponse
 from au import getResponse
 from job import Job
 
-     
-# what calls this because that is where we set the page to reload consistently.
+  
+  
 def index(request):
-    data = getResponse()
+    template = loader.get_template('splash.html')
+    
+    return HttpResponse(template.render())
+    
+    
+# what calls this because that is where we set the page to reload consistently.
+def webapp(request):
+    data, hierarchy = getResponse()
     dataset = []
-    dataOrder = ["name","latest_message","host","batch_sys_name","submit_method_id","submitted_time_string","started_time_string","finished_time_string","mean_elapsed_time"]
-
+    # dataOrder = sorted(hierarchy.keys())
+    # if(hierarchy == None):
+    #     d = Job()
+    #     d.junk_fill()
+    #     for i in range(10):
+    #         dataset.append(d.as_dict())
+    # else:
+    #     for job in data:
+    #         job = job.as_dict()
+    #     dataset = data
+        
+    # context = {
+    #     'dataOrderKey' : dataOrder,
+    #     'data' : dataset,
+    # }
+    # template = loader.get_template('index.html')
+    # return HttpResponse(template.render(context, request) )
+    
+    
+    dataOrder = ["name", "label", "latest_message","host","batch_sys_name","submit_method_id","submitted_time_string","started_time_string","finished_time_string","mean_elapsed_time"]
     if(data == None):
         d = Job()
         d.junk_fill()
